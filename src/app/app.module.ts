86fdecfd674d8776172262e4f6582c91957b2b3c
Parasmani Jain <parasmani.jain2208@gmail.com>
2025-11-29 19:33:40 +0530

    typescript packages added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { CitiesService } from './services/cities.service';
 import { UnlabelledClockComponent } from './components/unlabelled-clock/unlabelled-clock.component';
 
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    UnlabelledClockComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule
-  ],
-  providers: [CitiesService],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        UnlabelledClockComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule], providers: [CitiesService, provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
