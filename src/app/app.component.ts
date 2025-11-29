d1685de64a7ca4ca4edecee97c8099fa37e350b0
Parasmani Jain <parasmani.jain2208@gmail.com>
2025-11-29 19:45:08 +0530

    typescript packages added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { CitiesService } from './services/cities.service';
+import { Component, OnInit } from "@angular/core";
+import { CitiesService } from "./services/cities.service";
+import { UnlabelledClockComponent } from "./components/unlabelled-clock/unlabelled-clock.component";
+import { CommonModule } from "@angular/common";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
+  standalone: true,
+  imports: [UnlabelledClockComponent, CommonModule],
+  providers: [CitiesService],
 })
-
 export class AppComponent implements OnInit {
-
   public cities;
 
-  constructor(private citiesService: CitiesService) {
-  }
-    ngOnInit() {
+  constructor(private citiesService: CitiesService) {}
+  ngOnInit() {
     this.getCities();
   }
 
   getCities() {
-    this.citiesService.getCities()
-    .subscribe(res => {
+    this.citiesService.getCities().subscribe((res) => {
       this.cities = res;
-    } );
+    });
   }
 }
-
